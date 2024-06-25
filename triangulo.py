9f6bd1824afe06cadadbc8196f6f6eff089a50cc
Leeh <151933937+Leehzita@users.noreply.github.com>
2024-06-25 13:57:13 -0300

    Update triangulo.py

--- triangulo.py
+++ triangulo.py
@@ -2,4 +2,4 @@ print('Programa de cálculo de área do triângulo b.h/2')
 num1=int(input('Qual é a base? '))
 num2=int(input('Qual é a altura? '))
 area=(int(num1)*int(num2))/2
-print('A área do triângulo é de: ',area,'m²')0,
+print('A área do triângulo é de: ',area,'m²')
